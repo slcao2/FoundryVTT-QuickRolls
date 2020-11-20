d7dc3852ece344dc708185141048f37d9adce5e6
Steven <steven.l.cao@gmail.com>
2020-11-20 04:19:17 +0000

    auto roll attack on item card

--- scripts/overrides.js
+++ scripts/overrides.js
@@ -1,6 +1,185 @@
 import { debug } from "./utils/logger.js";
-import { getAttackRollFormula } from "./utils/roll.js";
-import { generateChatRollData } from "./chat.js";
+
+/**
+ * Place an attack roll using an item (weapon, feat, spell, or equipment)
+ * Rely upon the d20Roll logic for the core implementation
+ *
+ * @param {object} options        Roll options which are configured and provided to the d20Roll function
+ * @return {Promise<Roll|null>}   A Promise which resolves to the created Roll instance
+ */
+async function rollAttack(options={}) {
+  const itemData = this.data.data;
+  const actorData = this.actor.data.data;
+  const flags = this.actor.data.flags.dnd5e || {};
+  if ( !this.hasAttack ) {
+    throw new Error("You may not place an Attack Roll with this Item.");
+  }
+  const rollData = this.getRollData();
+
+  // Define Roll bonuses
+  const parts = [`@mod`];
+  if ( (this.data.type !== "weapon") || itemData.proficient ) {
+    parts.push("@prof");
+  }
+
+  // Attack Bonus
+  if ( itemData.attackBonus ) parts.push(itemData.attackBonus);
+  const actorBonus = actorData?.bonuses?.[itemData.actionType] || {};
+  if ( actorBonus.attack ) parts.push(actorBonus.attack);
+
+  // Ammunition Bonus
+  delete this._ammo;
+  const consume = itemData.consume;
+  if ( consume?.type === "ammo" ) {
+    const ammo = this.actor.items.get(consume.target);
+    if(ammo?.data){
+      const q = ammo.data.data.quantity;
+      const consumeAmount = consume.amount ?? 0;
+      if ( q && (q - consumeAmount >= 0) ) {
+        this._ammo = ammo;
+        let ammoBonus = ammo.data.data.attackBonus;
+        if ( ammoBonus ) {
+          parts.push("@ammo");
+          rollData["ammo"] = ammoBonus;
+        }
+      }
+    }
+  }
+
+  // Compose roll options
+  const event = options.event;
+
+  // Expanded critical hit thresholds
+  let critical = 20;
+  if (( this.data.type === "weapon" ) && flags.weaponCriticalThreshold) {
+    critical = parseInt(flags.weaponCriticalThreshold);
+  } else if (( this.data.type === "spell" ) && flags.spellCriticalThreshold) {
+    critical = parseInt(flags.spellCriticalThreshold);
+  }
+
+  // Elven Accuracy
+  let elvenAccuracy = false;
+  if ( ["weapon", "spell"].includes(this.data.type) ) {
+    if (flags.elvenAccuracy && ["dex", "int", "wis", "cha"].includes(this.abilityMod)) {
+      elvenAccuracy = true;
+    }
+  }
+
+  // Apply Halfling Lucky
+  let halflingLucky = false;
+  if ( flags.halflingLucky ) halflingLucky = true;
+
+
+  // Apply Reliable Talent
+  let reliableTalent = false;
+  if ( flags.reliableTalent ) reliableTalent = true;
+
+
+  // Prepare Message Data
+  parts.push("@bonus");
+
+  // Handle fast-forward events
+  let adv = 0;
+
+  // Define the inner roll function
+  const _roll = (parts, adv, form) => {
+
+    // Determine the d20 roll and modifiers
+    let nd = 1;
+    let mods = halflingLucky ? "r=1" : "";
+
+    // Handle advantage
+    if (adv === 1) {
+      nd = elvenAccuracy ? 3 : 2;
+      mods += "kh";
+    }
+
+    // Handle disadvantage
+    else if (adv === -1) {
+      nd = 2;
+      mods += "kl";
+    }
+
+    // Prepend the d20 roll
+    let formula = `${nd}d20${mods}`;
+    if (reliableTalent) formula = `{${nd}d20${mods},10}kh`;
+    parts.unshift(formula);
+
+    // Optionally include a situational bonus
+    if ( form ) {
+      rollData['bonus'] = form.bonus.value;
+    }
+    if (!rollData["bonus"]) parts.pop();
+
+    // Optionally include an ability score selection (used for tool checks)
+    const ability = form ? form.ability : null;
+    if (ability && ability.value) {
+      rollData.ability = ability.value;
+      const abl = rollData.abilities[rollData.ability];
+      if (abl) {
+        rollData.mod = abl.mod;
+      }
+    }
+
+    // Execute the roll
+    let roll = new Roll(parts.join(" + "), rollData);
+    try {
+      roll.roll();
+    } catch (err) {
+      console.error(err);
+      ui.notifications.error(`Dice roll evaluation failed: ${err.message}`);
+      return null;
+    }
+
+    // Flag d20 options for any 20-sided dice in the roll
+    let fumble = 1;
+    let targetValue = null;
+    for (let d of roll.dice) {
+      if (d.faces === 20) {
+        d.options.critical = critical;
+        d.options.fumble = fumble;
+        if (targetValue) d.options.target = targetValue;
+      }
+    }
+
+    return roll;
+  };
+
+  // Create the Roll instance
+  const attackRoll = _roll.bind(this)(parts, adv);
+
+  if ( attackRoll === false ) return null;
+
+  // Handle resource consumption if the attack roll was made
+  const allowed = await this._handleResourceConsumption({isCard: false, isAttack: true});
+  if ( allowed === false ) return null;
+
+  if (game.dice3d) {
+    game.dice3d.showForRoll(attackRoll);
+  }
+
+  // Get messageId
+  let message = options.message;
+  if (!message) {
+    const button = event.currentTarget;
+    const card = button.closest(".chat-card");
+    const messageId = card.closest(".message").dataset.messageId;
+    message =  game.messages.get(messageId);
+  }
+
+  // Replace button with roll
+  const content = duplicate(message.data.content);
+  const attackRegex = /<button data-action="attack">[^<]*<\/button>/;
+
+  const attackRollHtml = await attackRoll.render();
+  const headerString = game.i18n.localize("DND5E.Attack");
+  const updateString = `<h4 class="quick-rolls-card-button-header">${headerString}</h4>${attackRollHtml}`;
+
+  const replacedContent = content.replace(attackRegex, updateString);
+  message.update({ content: replacedContent });
+
+  return attackRoll;
+}
 
 /**
  * Roll the item to Chat, creating a chat card which contains follow up attack or damage roll options
@@ -10,14 +189,8 @@ import { generateChatRollData } from "./chat.js";
  *                                        the prepared chat message data (if false).
  * @return {Promise}
  */
-export async function rollItem({configureDialog=true, rollMode=null, createMessage=true}={}) {
+async function rollItem({configureDialog=true, rollMode=null, createMessage=true}={}) {
   debug("this", this);
-
-  const attackRollFormula = getAttackRollFormula(this.getRollData());
-  const attackRoll = new Roll(attackRollFormula);
-  const advDisRoll = new Roll(attackRollFormula);
-  attackRoll.evaluate();
-  advDisRoll.evaluate();
 
   // Basic template rendering data
   const token = this.actor.token;
@@ -34,8 +207,6 @@ export async function rollItem({configureDialog=true, rollMode=null, createMessa
     isSpell: this.data.type === "spell",
     hasSave: this.hasSave,
     hasAreaTarget: this.hasAreaTarget,
-    attackRoll: generateChatRollData(attackRoll),
-    advDisRoll: generateChatRollData(advDisRoll),
   };
 
   // For feature items, optionally show an ability usage dialog
@@ -80,15 +251,15 @@ export async function rollItem({configureDialog=true, rollMode=null, createMessa
   if ( ["gmroll", "blindroll"].includes(rollMode) ) chatData["whisper"] = ChatMessage.getWhisperRecipients("GM");
   if ( rollMode === "blindroll" ) chatData["blind"] = true;
 
-  // Roll 3D dice on screen if Dice So Nice is enabled
-  if (game.dice3d) {
-    game.dice3d.showForRoll(attackRoll);
-    game.dice3d.showForRoll(advDisRoll);
-  }
-
   // Create the chat message
-  if ( createMessage ) return ChatMessage.create(chatData);
-  else return chatData;
+  if ( createMessage ) {
+    const message = await ChatMessage.create(chatData);
+    
+    if (this.hasAttack) {
+      this.rollAttack.bind(this)({ message })
+    }
+    return message;
+  } else return chatData;
 };
 
 /**
@@ -100,8 +271,7 @@ export async function rollItem({configureDialog=true, rollMode=null, createMessa
  * @param {object} [options]      Additional options passed to the damageRoll function
  * @return {Promise<Roll>}        A Promise which resolves to the created Roll instance
  */
-export async function rollDamage({event, spellLevel=null, versatile=false, options={}}={}) {
-  
+async function rollDamage({event, spellLevel=null, versatile=false, options={}}={}) {
   if ( !this.hasDamage ) throw new Error("You may not make a Damage Roll with this Item.");
   const itemData = this.data.data;
   const actorData = this.actor.data.data;
@@ -144,7 +314,6 @@ export async function rollDamage({event, spellLevel=null, versatile=false, optio
   }
 
   // Prepare Message Data
-  // parts = parts.concat(["@bonus"]);
   parts.push("@bonus");
 
   // Define inner roll function
@@ -203,7 +372,12 @@ export async function rollDamage({event, spellLevel=null, versatile=false, optio
   const versatileRegex = /<button data-action="versatile">[^<]*<\/button>/;
 
   const damageRollHtml = await damageRoll.render();
-  const headerString = versatile ? "Versatile" : "Damage";
+  let headerString = game.i18n.localize("DND5E.Damage");
+  if (versatile) {
+    headerString = game.i18n.localize("DND5E.Versatile");
+  } else if (this.isHealing) {
+    headerString = game.i18n.localize("DND5E.Healing");
+  }
   const updateString = `<h4 class="quick-rolls-card-button-header">${headerString}</h4>${damageRollHtml}`;
   const replaceRegex = versatile ? versatileRegex : damageRegex;
 
@@ -215,5 +389,6 @@ export async function rollDamage({event, spellLevel=null, versatile=false, optio
 
 export const overrideRoller = () => {
   CONFIG.Item.entityClass.prototype.roll = rollItem;
+  CONFIG.Item.entityClass.prototype.rollAttack = rollAttack;
   CONFIG.Item.entityClass.prototype.rollDamage = rollDamage;
 };
