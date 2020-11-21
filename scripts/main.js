3813831f9fb0c0c3c368d3203d2b2b79c666ed9a
slcao2 <slcao2@illinois.edu>
2020-11-20 22:28:05 -0600

    removing unused code

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 import { registerSettings } from './settings.js';
-import { debug, info } from './utils/logger.js';
-import { handleItemClick } from './chat.js';
+import { debug } from './utils/logger.js';
 import { overrideRoller } from './overrides.js';
 
 export const TEMPLATE_PATH_PREFIX = "modules/quick-rolls/templates";
@@ -42,41 +41,3 @@ Hooks.on("setup", () => {
 Hooks.on("ready", () => {
   debug("ready");
 });
-
-// Hooks.on("preCreateChatMessage", (data, flags, user) => {
-//   debug("preCreateChatMessage", data, flags, user);
-// });
-
-// Hooks.on("createChatMessage", (chatMessage, flags, user) => {
-//   debug("createChatMessage", chatMessage, flags, user);
-// });
-
-// Hooks.on("renderChatMessage", (chatMessage, html, data) => {
-//   debug("renderChatMessage", chatMessage, html, data);
-  
-//   // const actorId = $(chatMessage.data.content).data("actor-id");
-//   // const itemId = $(chatMessage.data.content).data("item-id");
-//   // if (!actorId && !itemId) {
-//   //   return;
-//   // }
-
-//   // const actor = game.actors.get(actorId);
-//   // const item = actor.getOwnedItem(itemId); 
-//   // debug("item", item);
-//   // const rollData = item.getRollData();
-//   // debug("item roll data", item.getRollData())
-
-//   // const attackRollFormula = getAttackRollFormula(rollData);
-//   // debug(attackRollFormula);
-
-//   // const rollOne = new Roll(attackRollFormula);
-//   // const rollTwo = new Roll(attackRollFormula);
-
-//   // rollOne.evaluate();
-//   // rollTwo.evaluate();
-
-//   // debug(rollOne);
-//   // debug(rollTwo);
-
-//   handleItemClick(chatMessage, html, data);
-// });
--- scripts/models/DiceRoll.js
+++
@@ -1,7 +0,0 @@
-class DiceRoll {
-  constructor(rollFormula, rollParts, rollTotal) {
-    this.rollFormula = rollFormula;
-    this.rollParts = rollParts;
-    this.rollTotal = rollTotal;
-  }
-}
