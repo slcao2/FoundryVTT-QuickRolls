b03ce9ee005d17bb48336b8f2f9e40902fa53caf
slcao2 <slcao2@illinois.edu>
2020-11-22 23:47:11 -0600

    refactoring

--- scripts/itemOverrides.js
+++ scripts/itemOverrides.js
@@ -1,5 +1,5 @@
 import { debug } from "./utils/logger.js";
-import { moduleName, SETTING_CRIT_CALCULATION, CRIT_CALCULATION_DEFAULT, CRIT_CALCULATION_MAXCRITDICE } from "./settings.js";
+import { calculateCrit, modifyChatHtml, modifyRollHtml } from "./utils/chat.js";
 
 /**
  * Place an attack roll using an item (weapon, feat, spell, or equipment)
@@ -273,33 +273,6 @@ async function rollItem({configureDialog=true, rollMode=null, createMessage=true
   } else return chatData;
 };
 
-function calculateCrit({ parts, rollData, roll, criticalMultiplier, criticalBonusDice }) {
-  const critType = game.settings.get(moduleName, SETTING_CRIT_CALCULATION);
-  switch (critType) {
-    case CRIT_CALCULATION_DEFAULT:
-      roll.alter(criticalMultiplier, 0);      // Multiply all dice
-      if ( roll.terms[0] instanceof Die ) {   // Add bonus dice for only the main dice term
-        roll.terms[0].alter(1, criticalBonusDice);
-        roll._formula = roll.formula;
-      }
-      break;
-    case CRIT_CALCULATION_MAXCRITDICE:
-      parts.push("@crit");
-      rollData["crit"] = 0;
-      const dRegex = /[0-9]*d[0-9]+/;
-      parts.forEach(part => {
-        part.split("+").map(p => p.trim()).forEach(p => {
-          if (dRegex.test(p)) {
-            rollData["crit"] += p.split("d").reduce((acc, curr) => acc * curr, 1)
-          }
-        });
-      });
-      roll = new Roll(parts.join("+"), rollData);
-      break;
-  }
-  return roll;
-}
-
 /**
  * Place a damage roll using an item (weapon, feat, spell, or equipment)
  * Rely upon the damageRoll logic for the core implementation.
@@ -434,51 +407,6 @@ async function rollFormula({event, spellLevel, message}) {
   this.replaceButton({ headerKey, headerRegex, buttonRegex, message, roll, action });
 
   return roll;
-}
-
-function modifyRollHtml({ rollHtml, roll, action, message }) {
-  const html = $(rollHtml);
-  switch (action) {
-    case "attack":
-      if (message.isAttackCritical) {
-        html.find(".dice-total").addClass("critical");
-      } else if (message.isAttackFumble) {
-        html.find(".dice-total").addClass("fumble");
-      }
-      break;
-    case "vantage":
-      if (message.isVantageCritical) {
-        html.find(".dice-total").addClass("critical");
-      } else if (message.isVantageFumble) {
-        html.find(".dice-total").addClass("fumble");
-      }
-      break;
-  }
-  html.first().addClass(`qr-${action}`);
-  return html.prop("outerHTML");
-}
-
-function modifyChatHtml({ chatHtml, message, action }) {
-  const html = $(chatHtml);
-
-  switch (action) {
-    case "attack":
-      message.isCritical = message.isAttackCritical;
-      message.isFumble = message.isAttackFumble;
-    case "vantage":
-      if ((message.isAdvantage && message.attackRollTotal >= message.vantageRollTotal) || (!message.isAdvantage && message.attackRollTotal <= message.vantageRollTotal)) {
-        html.find(".qr-vantage").addClass("qr-discarded");
-        message.isCritical = message.isAttackCritical;
-        message.isFumble = message.isAttackFumble;
-      } else if ((message.isAdvantage && message.attackRollTotal < message.vantageRollTotal) || (!message.isAdvantage && message.attackRollTotal > message.vantageRollTotal)) {
-        html.find(".qr-attack").addClass("qr-discarded");
-        message.isCritical = message.isVantageCritical;
-        message.isFumble = message.isVantageFumble;
-      }
-      break;
-  }
-
-  return html.prop("outerHTML");
 }
 
 async function replaceButton({ headerKey, buttonRegex, headerRegex, message, roll, action }) {
---
+++ scripts/utils/chat.js
@@ -0,0 +1,73 @@
+import { moduleName, SETTING_CRIT_CALCULATION, CRIT_CALCULATION_DEFAULT, CRIT_CALCULATION_MAXCRITDICE } from "../settings.js";
+
+export function calculateCrit({ parts, rollData, roll, criticalMultiplier, criticalBonusDice }) {
+  const critType = game.settings.get(moduleName, SETTING_CRIT_CALCULATION);
+  switch (critType) {
+    case CRIT_CALCULATION_DEFAULT:
+      roll.alter(criticalMultiplier, 0);      // Multiply all dice
+      if ( roll.terms[0] instanceof Die ) {   // Add bonus dice for only the main dice term
+        roll.terms[0].alter(1, criticalBonusDice);
+        roll._formula = roll.formula;
+      }
+      break;
+    case CRIT_CALCULATION_MAXCRITDICE:
+      parts.push("@crit");
+      rollData["crit"] = 0;
+      const dRegex = /[0-9]*d[0-9]+/;
+      parts.forEach(part => {
+        part.split("+").map(p => p.trim()).forEach(p => {
+          if (dRegex.test(p)) {
+            rollData["crit"] += p.split("d").reduce((acc, curr) => acc * curr, 1)
+          }
+        });
+      });
+      roll = new Roll(parts.join("+"), rollData);
+      break;
+  }
+  return roll;
+}
+
+export function modifyChatHtml({ chatHtml, message, action }) {
+  const html = $(chatHtml);
+
+  switch (action) {
+    case "attack":
+      message.isCritical = message.isAttackCritical;
+      message.isFumble = message.isAttackFumble;
+    case "vantage":
+      if ((message.isAdvantage && message.attackRollTotal >= message.vantageRollTotal) || (!message.isAdvantage && message.attackRollTotal <= message.vantageRollTotal)) {
+        html.find(".qr-vantage").addClass("qr-discarded");
+        message.isCritical = message.isAttackCritical;
+        message.isFumble = message.isAttackFumble;
+      } else if ((message.isAdvantage && message.attackRollTotal < message.vantageRollTotal) || (!message.isAdvantage && message.attackRollTotal > message.vantageRollTotal)) {
+        html.find(".qr-attack").addClass("qr-discarded");
+        message.isCritical = message.isVantageCritical;
+        message.isFumble = message.isVantageFumble;
+      }
+      break;
+  }
+
+  return html.prop("outerHTML");
+}
+
+export function modifyRollHtml({ rollHtml, roll, action, message }) {
+  const html = $(rollHtml);
+  switch (action) {
+    case "attack":
+      if (message.isAttackCritical) {
+        html.find(".dice-total").addClass("critical");
+      } else if (message.isAttackFumble) {
+        html.find(".dice-total").addClass("fumble");
+      }
+      break;
+    case "vantage":
+      if (message.isVantageCritical) {
+        html.find(".dice-total").addClass("critical");
+      } else if (message.isVantageFumble) {
+        html.find(".dice-total").addClass("fumble");
+      }
+      break;
+  }
+  html.first().addClass(`qr-${action}`);
+  return html.prop("outerHTML");
+}
