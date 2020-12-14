f59ceaa638d73dfa280ca5d1ea2d63840a7448bb
slcao2 <slcao2@illinois.edu>
2020-12-14 16:44:38 -0600

    show total for damage rolls

--- scripts/utils/chat.js
+++ scripts/utils/chat.js
@@ -108,6 +108,7 @@ export const buildDamageRollHtmlNode = async ({ rolls, types }) => {
         }
       ))).flat(),
     })),
+    total: rolls.reduce((acc, r) => acc + r.total, 0),
   };
   const node = await renderTemplate(`${TEMPLATE_PATH_PREFIX}/dice-roll.html`, templateData);
   return $(node);
